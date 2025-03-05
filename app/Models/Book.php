4b2b101119ed05f83484286dba98e40790819bc3
Mansur <ttytkhkttymi@gmail.com>
2025-03-05 11:30:11 +0500

    Added one-to-many relationship between User and Book models

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Book extends Model
 {
-    //
+    protected $fillable = [
+        'title',
+        'description',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -51,4 +51,9 @@ class User extends Authenticatable
     {
         return $this->morphOne(Image::class, 'imageable');
     }
+
+    public function books()
+    {
+        return $this->hasMany(Book::class);
+    }
 }
