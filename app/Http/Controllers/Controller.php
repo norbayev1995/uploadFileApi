7b786a2bc54f064f2ebce04abc18723780944e3c
Mansur <ttytkhkttymi@gmail.com>
2025-03-05 10:24:07 +0500

    Added file upload and delete methods in Controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,5 +4,17 @@ namespace App\Http\Controllers;
 
 abstract class Controller
 {
-    //
+    public function uploadFile($file, $path = "uploads")
+    {
+        $photoName = md5(time() . $file->getFilename()) . "." . $file->getClientOriginalExtension();
+        return $file->storeAs($path, $photoName, 'public');
+    }
+
+    public function deletePhoto($path)
+    {
+        $fullPath = storage_path("app/public/" . $path);
+        if (file_exists($fullPath)) {
+            unlink($fullPath);
+        }
+    }
 }
